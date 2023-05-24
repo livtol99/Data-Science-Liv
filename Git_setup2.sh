27df657e025dd535030a8d3b93006fcc0d031b9f
livtol99 <liv.tollanes@gmail.com>
2023-05-24 09:44:49 +0200

    all work so far

---
+++ Git_setup2.sh
@@ -0,0 +1,6 @@
+# run using
+# bash path_to_folder/git_setup2.sh
+#!/bin/bash
+git config --global user.email "liv.tollanes@gmail.com"
+git config --global user.name "livtol99"
+git config --global user.password "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"
