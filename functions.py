906a3eb40206dd01b60a840b42b266c38d1c1e09
livtol99 <liv.tollanes@gmail.com>
2023-05-25 17:45:39 +0200

    Functioning loop to create cluster forecasts

--- functions.py
+++ functions.py
@@ -32,61 +32,61 @@ def create_splits(df):
     return train_df, test_df
 
 
-def yr_aggregate_train(train_df):
+def create_representative_train(train_df):
     # Perform yearly aggregation across all individual time series
-    agg_df_train = train_df.groupby('Year')['LE'].median().reset_index()
-    agg_df_train.rename({'Year':'ds', 'LE':'y'}, axis=1, inplace=True)
+    representative_df_train = train_df.groupby('Year')['LE'].median().reset_index()
+    representative_df_train.rename({'Year':'ds', 'LE':'y'}, axis=1, inplace=True)
     
-    return agg_df_train
+    return representative_df_train
 
-def yr_aggregate_test(test_df):
+def create_representative_test(test_df):
     # Perform yearly aggregation across all individual time series
-    agg_df_test = test_df.groupby('Year')['LE'].median().reset_index()
-    agg_df_test.rename({'Year':'ds', 'LE':'y'}, axis=1,inplace=True)
-    return agg_df_test
+    representative_df_test = test_df.groupby('Year')['LE'].median().reset_index()
+    representative_df_test.rename({'Year':'ds', 'LE':'y'}, axis=1,inplace=True)
+    return representative_df_test
 
-def rename_merge_concat(train_df, test_df, agg_df_train, agg_df_test, predictions):
-    # Rename columns to fix the issues
-    agg_df_train.rename({'y':'y_agg_tr'}, axis=1, inplace=True)
-    agg_df_test.rename({'y':'y_agg_tr'}, axis=1, inplace=True)
+def create_adjustment_series(train_df, test_df, representative_df_train, representative_df_test, representative_forecasts):
+    # Rename columns to avoid issues during merge
+    representative_df_train.rename({'y':'y_repr_tr'}, axis=1, inplace=True)
+    representative_df_test.rename({'y':'y_repr_tst'}, axis=1, inplace=True)
     train_df.rename({'Year':'ds','LE':'y_tr'}, axis=1, inplace=True)
     test_df.rename({'Year':'ds', 'LE':'y_tst'}, axis=1, inplace=True)
 
-    # Merge train_df and aggregated train df
-    merged_train_df = pd.merge(train_df, agg_df_train, on='ds', how='inner')
+    # Merge train_df and representative train df
+    merged_train_df = pd.merge(train_df, representative_df_train, on='ds', how='inner')
 
-    # Compute adjustment series for the merged test and train data sets
-    merged_train_df['delta_val'] = merged_train_df['y_tr'] - merged_train_df['y_agg_tr']
+    # Compute adjustment series for the train data set by subtracting the representative ts from the actual ts
+    merged_train_df['adjusted_val'] = merged_train_df['y_tr'] - merged_train_df['y_repr_tr']
 
-    # Merge the "yhat" column from "predictions" with "test_df"
-    merged_test_df = pd.merge(test_df, predictions[['ds', 'yhat']], on='ds', how='inner')
+    # Merge the "yhat" (representative forecasts) column  with the est_df
+    merged_test_df = pd.merge(test_df, representative_forecasts[['ds', 'yhat']], on='ds', how='inner')
 
     # Concatenate merged_train_df and merged_test_df
     df_actual_all = pd.concat([merged_train_df, merged_test_df], ignore_index=True)
 
-    return merged_train_df, merged_test_df, df_actual_all
+    return df_actual_all
 
-def get_linear_model_pred(train_test_df, test_dat):
-    # Sort the train_test_df based on the date column
-    train_test_df = train_test_df.sort_values(['ds']).reset_index(drop=True)
+def get_linear_model_pred(df_actual_all, test_dat):
+    # Sort the df_actual_all based on the date column
+    df_actual_all = df_actual_all.sort_values(['ds']).reset_index(drop=True)
 
-    # Create a column 't' with values from 0 to the number of rows in train_test_df
-    train_test_df['t'] = np.arange(0, train_test_df.shape[0])
+    # Create a column 't' with values from 0 to the number of rows in df_actual_all
+    df_actual_all['t'] = np.arange(0, df_actual_all.shape[0])
 
     # Create a column 't2' which is the square of the 't' values
-    train_test_df['t2'] = train_test_df['t'] ** 2
+    df_actual_all['t2'] = df_actual_all['t'] ** 2
 
-    # Extract the training data (excluding test period) from train_test_df
-    train_df = train_test_df.loc[~train_test_df['ds'].astype(str).isin(test_dat['ds'].astype(str).values.tolist())].reset_index(drop=True)
+    # Extract the training data (excluding test period) from df_actual_all
+    train_df = df_actual_all.loc[~df_actual_all['ds'].astype(str).isin(test_dat['ds'].astype(str).values.tolist())].reset_index(drop=True)
 
-    # Extract the test data from train_test_df
-    test_df = train_test_df.loc[train_test_df['ds'].astype(str).isin(test_dat['ds'].astype(str).values.tolist())].reset_index(drop=True)
+    # Extract the test data from df_actual_all
+    test_df = df_actual_all.loc[df_actual_all['ds'].astype(str).isin(test_dat['ds'].astype(str).values.tolist())].reset_index(drop=True)
 
     # Create a LinearRegression object and fit the model using the training data
-    lm_obj = LinearRegression().fit(train_df[['t', 't2']], train_df['delta_val'])
+    lm_obj = LinearRegression().fit(train_df[['t', 't2']], train_df['adjusted_val'])
 
     # Make predictions for the test data
-    test_delta_pred = lm_obj.predict(test_df[['t', 't2']])
+    adjustment_Forecasts_test = lm_obj.predict(test_df[['t', 't2']])
 
     # Create a DataFrame with the predicted values and corresponding months in the test period
-    return pd.DataFrame({'month': test_df['ds'].values, 'test_delta_pred': test_delta_pred})
+    return pd.DataFrame({'Year': test_df['ds'].values, 'adjustment_Forecasts_test': adjustment_Forecasts_test})
